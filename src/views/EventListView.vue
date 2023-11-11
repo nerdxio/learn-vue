b3fbff40192befc1ca50d7e972ef7fbc555dfb71
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-11-11 08:35:13 +0200

    Add axios dependency and update routing

---
+++ src/views/EventListView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref(null)
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events')
+    .then((response) => {
+     events.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
